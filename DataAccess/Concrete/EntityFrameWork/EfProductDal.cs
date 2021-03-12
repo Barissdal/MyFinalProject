b9c8128212ae73cde9d1b73e453f2bd6c9a50419
Barış Dal <63496242+Barissdal@users.noreply.github.com>
2021-03-12 13:51:40 +0300

    12.03.2021

--- DataAccess/Concrete/EntityFrameWork/EfProductDal.cs
+++ DataAccess/Concrete/EntityFrameWork/EfProductDal.cs
@@ -6,13 +6,31 @@ using System.Text;
 using Core.DataAccess.EntityFrameWork;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFrameWork
 {
     //NuGet
-    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto {
+                             ProductId = p.ProductId,
+                             ProductName = p.ProductName,
+                             CategoryName = c.CategoryName,
+                             UnitsInStock = p.UnitsInStock};
+
+                // result çünkü IQuaryable oyüzden list olarak döndürmeliyiz.
+                return result.ToList();
+
+            }
+        }
     }
 }
