3a0a7fec1423735c79a6f15b1626a4ab1a57923a
Barış Dal <63496242+Barissdal@users.noreply.github.com>
2021-03-16 15:53:06 +0300

    16.03.2021 Study

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFrameWork;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    // Route isteğin hangi yolla geleceğini ifade eder.
+    // buradaki controller , products 'tır.
+    [Route("api/[controller]")]
+    // ATTRIBUTE (JAVA'DA ANNOTATION)
+    // Attribute, bir class la ilgili bilgi vermektir.
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled = Gevşek bağlılık, yani baplılığı var ama soyuta bağlı
+        //naming convention
+        //IoC Container = Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain 
+
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
