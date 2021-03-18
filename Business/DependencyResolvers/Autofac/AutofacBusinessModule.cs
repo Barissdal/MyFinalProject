f8a25bd34f39c4ff7ed608cde1ba9434ca94ebe4
Barış Dal <63496242+Barissdal@users.noreply.github.com>
2021-03-18 11:44:33 +0300

    18.03.2021 Commit1

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFrameWork;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // birisi senden IProductService isterse ona bir tabe ProductManager örneği ver.
+            // SingleInstance, tek bir instance oluşturmak için.
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
