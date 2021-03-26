0b9960fa56baa46fcc1bfe142de07d5b1d88c494
Barış Dal <63496242+Barissdal@users.noreply.github.com>
2021-03-26 17:35:15 +0300

    14.Gün Ders

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.DataAccess;
+using Core.Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        //burada join işlemi yapacağız
+        //kullanıcının veritabanından claim lerini çekmek için
+        List<OperationClaim> GetClaims(User user);
+    }
+}
