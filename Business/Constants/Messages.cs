69f2ef4297f488a25be94a0dc550a91cbee1ea05
Barış Dal <63496242+Barissdal@users.noreply.github.com>
2021-03-29 11:51:51 +0300

    14.Ders 2

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
+using Core.Entities.Concrete;
 using Entities.Concrete;
 
 namespace Business.Constants
@@ -16,5 +17,12 @@ namespace Business.Constants
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
         public static string CategoryCountOfError = "Kategori sayısı 15'i geçemez.";
         public static string AuthorizationDenied = "Yetkiniz yok";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
     }
 }
