edb17b13d0f3e1f6a2cb749dc49fab07434d2882
Barış Dal <63496242+Barissdal@users.noreply.github.com>
2021-04-18 18:24:41 +0300

    20.Gün Ders

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,7 +31,7 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
-        [SecuredOperation("product.add,admin")]
+        //[SecuredOperation("product.add,admin")]
         //Add metodunu ProductValidator ile validation test et
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
